8069fe1690ed2ce7df24c3cc628dcd5279c9b6ff
Tony Miller <mcfiredrill@gmail.com>
2020-05-30 16:57:19 -0700

    set CHAT_SOCKET_URL

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,8 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+    CHAT_SOCKET_URL: process.env.CHAT_SOCKET_URL,
   };
 
   if (environment === 'development') {

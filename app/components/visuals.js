1e7ad66f670a54cc00d3070af02e348d496e9487
Tony Miller <mcfiredrill@gmail.com>
2020-05-30 18:25:02 -0700

    add visuals

---
+++ app/components/visuals.js
@@ -0,0 +1,32 @@
+import classic from 'ember-classic-decorator';
+import { classNames } from '@ember-decorators/component';
+import { inject as service } from '@ember/service';
+import { oneWay } from '@ember/object/computed';
+import Component from '@ember/component';
+import { later } from '@ember/runloop';
+
+@classic
+@classNames('visuals')
+export default class DatafruitsVisuals extends Component {
+  @service
+  videoStream;
+
+  @oneWay('videoStream.active')
+  videoStreamActive;
+
+  didRender() {
+    if(!this.get('fastboot.isFastBoot')){
+      if(this.videoStreamActive){
+        this.videoStream.initializePlayer();
+      }else {
+        later(()=> {
+          this.videoStream.fetchStream();
+        }, 15000);
+      }
+    }
+  }
+
+  didInsertElement() {
+    this.videoStream.fetchStream();
+  }
+}
--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function(/* env */) {
   return {
-    clientAllowedKeys: ['CHAT_SOCKET_URL'],
+    clientAllowedKeys: ['CHAT_SOCKET_URL', 'STREAM_HOST', 'STREAM_NAME'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env')
